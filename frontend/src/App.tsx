48599614e406821d8672c274a5565b26bef0222c
MoriMokata <b6214562@g.sut.ac.th>
2021-11-02 17:18:12 +0700

    backend G11

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import Body from "./components/Body";
-import Navbar from "./components/Navbar";
-import History from "./components/History";
+import Body from "./components/LabResult/Body";
+import Navbar from "./components/LabResult/Navbar";
+import History from "./components/LabResult/History";
 import SignIn from "./components/SignIn";
+import PreloadScreenings from './components/Screening/HistoryScreening';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CreateScreening from './components/Screening/CreateScreening';
+import NavbarNurse from "./components/Screening/NavbarNurse";
 
-import Home from './components/Home';
+import Home from './components/LabResult/Home';
 
 function App() {
   const [token, setToken] = useState<string>("");
@@ -21,15 +24,42 @@ function App() {
     return <SignIn />
   }
   
-let CheckRole = localStorage.getItem("Role");
+let CheckRole = String(localStorage.getItem("Role"));
 
   return (
     <div>
       <Router>
-        {token && (
-          {}
-
-        )}
+        {token && CheckRole === "MedicalTech" ? (
+          <>
+           <Fragment>
+            <Navbar/>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/History" component={History} />
+              <Route exact path="/link/body" component={Body} />
+            </Switch>
+          </Fragment>
+          </>
+        ) : (CheckRole === "Nurse" ? (
+          <>
+            <Fragment>
+            <NavbarNurse />
+            <Switch>
+              <Route exact path="/" component={PreloadScreenings} />
+              <Route exact path="/CreateScreening" component={CreateScreening} />
+            </Switch>
+          </Fragment>
+          </>
+        ) : (CheckRole === "Doctor" ? (
+          <>
+            <Navbar />
+          </>
+        ) : (CheckRole === "MedicalRecordOfficer" ? (
+          <>
+            <Navbar />
+          </>
+        ) : (<>{/* else condition */}</>))))
+        }
       </Router>
     </div>
 
